4ab5b1b910c4d02de5ce046343b663e04ead99f0
calyun <thisishaoyu@gmail.com>
2017-10-07 07:19:22 -0700

    Expanded scope of BrowserSync triggers

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,7 @@ var sass        = require('gulp-sass');
 var browserSync = require('browser-sync');
 var nodemon = require('gulp-nodemon');
 
-// we'd need a slight delay to reload browsers
-// connected to browser-sync after restarting nodemon
+// delay browser reloading after nodemon start
 var BROWSER_SYNC_RELOAD_DELAY = 500;
 
 gulp.task('nodemon', function (cb) {
@@ -47,29 +46,38 @@ gulp.task('browser-sync', ['nodemon'], function () {
     // informs browser-sync to use the following port for the proxied app
     // notice that the default port is 3000, which would clash with our expressjs
     port: 4000,
-
-    // open the proxied app in chrome
-    browser: ['google-chrome']
   });
 });
 
-gulp.task('js',  function () {
+// gulp.task('js-root',  function () {
+//   return gulp.src('*.js')
+//     // do stuff to JavaScript files
+//     //.pipe(uglify())
+//     //.pipe(gulp.dest('...'));
+// });
+
+gulp.task('js-public',  function () {
   return gulp.src('public/**/*.js')
-    // do stuff to JavaScript files
-    //.pipe(uglify())
-    //.pipe(gulp.dest('...'));
 });
 
 gulp.task('js-routes',  function () {
   return gulp.src('routes/*.js')
 });
 
+gulp.task('ejs-views',  function () {
+  return gulp.src('views/*.ejs')
+});
+
+gulp.task('ejs-views-partials',  function () {
+  return gulp.src('views/**/*.ejs')
+});
+
 gulp.task('sass',  function () {
-  return gulp.src('public/**/*.sass')
+  return gulp.src('public/stylesheets/*.sass')
 });
 
 gulp.task('css', function () {
-  return gulp.src('public/**/*.css')
+  return gulp.src('public/stylesheets/*.css')
     .pipe(browserSync.reload({ stream: true }));
 })
 
@@ -78,9 +86,12 @@ gulp.task('bs-reload', function () {
 });
 
 gulp.task('default', ['browser-sync'], function () {
-  gulp.watch('public/**/*.js',   ['js', browserSync.reload]);
-  gulp.watch('public/**/*.css',  ['css']);
-  gulp.watch('public/**/*.html', ['bs-reload']);
+  //gulp.watch("*.js", ['js-root', browserSync.reload]);
+  gulp.watch("public/**/*.js",   ['js-public', browserSync.reload]);
+  gulp.watch("public/stylesheets/*.css",  ['css']);
+  gulp.watch("public/**/*.html", ['bs-reload']);
   gulp.watch("public/stylesheets/*.sass", ['sass']);
   gulp.watch("routes/*.js", ['js-routes', browserSync.reload]);
+  gulp.watch("views/*.ejs", ['ejs-views', browserSync.reload]);
+  gulp.watch("views/**/*.ejs", ['ejs-views-partials', browserSync.reload]);
 });
