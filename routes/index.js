a9684306e1a068ddb03a294355e5f9903c8e4a10
calyun <thisishaoyu@gmail.com>
2017-10-07 07:10:54 -0700

    Add About and Help routes and pages

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('index', { result: "nothing yet"});
+	res.render('index', { result: "nothing yet" });
+});
+
+router.get('/about', function(req, res, next) {
+    res.render('about', { result: "nothing yet" });
+});
+
+router.get('/help', function(req, res, next) {
+    res.render('help', { result: "nothing yet" });
 });
 
 module.exports = router;
