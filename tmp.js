6a0bb04547dbc4c3b51082c6381437d78f077b4a
calyun <thisishaoyu@gmail.com>
2017-10-08 10:30:20 -0700

    Formatting

--- tmp.js
+++ tmp.js
@@ -2,7 +2,7 @@ ions.push({lat:xy[0], lng:xy[1]})
       }
 
       <script src="javascripts/MarkerClusterer.js"> </script>
-       <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJ3b4duXGP-7mSiQp-WG8DElJAE-bSs0M">  </script>
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJ3b4duXGP-7mSiQp-WG8DElJAE-bSs0M">  </script>
 
       function getLocs(){
           var map = new google.maps.Map(document.getElementById('map'), {
